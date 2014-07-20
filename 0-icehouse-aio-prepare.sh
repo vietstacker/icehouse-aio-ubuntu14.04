9de6c301282fe12203ccdf2ed91cd6a73aabbe76
congto <tcvn1985@gmail.com>
2014-07-20 20:44:56 +0700

    Sửa repos cho Ubuntu14.04

--- 0-icehouse-aio-prepare.sh
+++ 0-icehouse-aio-prepare.sh
@@ -34,8 +34,7 @@ EOF
 
 echo "########## Thu hien update he thong truoc khi cai dat ##########"
 
-apt-get install -y python-software-properties &&  add-apt-repository cloud-archive:icehouse -y 
-
+# apt-get install -y python-software-properties &&  add-apt-repository cloud-archive:icehouse -y 
 apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
 
 
